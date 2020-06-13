6af54148de44a741f79619cb2fdec6f8ee0fa114
Luis Chitala <53163045+luischitala@users.noreply.github.com>
2020-06-13 14:14:31 -0500

    Cambio de config a conf

--- movapp/urls.py
+++ movapp/urls.py
@@ -1,8 +1,8 @@
 
 from django.contrib import admin
 from django.urls import path
-from django.config import settings 
-from django.config.urls.static import static
+from django.conf import settings 
+from django.conf.urls.static import static
 
 from movapp import views as local_views
 from rutas import views as rutas_views
